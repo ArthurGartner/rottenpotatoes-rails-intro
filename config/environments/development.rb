06215f8496490a1a01d397dc2bce627c6fe2e207
Arthur Gartner <agartner@citadel.edu>
2020-09-28 02:29:01 +0000

    test commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-    config.hosts = nil
+  config.hosts = nil
     
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
